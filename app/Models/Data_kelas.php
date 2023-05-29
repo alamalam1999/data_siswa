b59eeb6d75501e296e971e887e8214b06cf3bf3c
adi alam <adikuasa.mangkualam@ist.id>
2023-05-29 17:52:38 +0700

    add file

---
+++ app/Models/Data_kelas.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Data_kelas extends Model
+{
+
+    public $incrementing = false;
+
+    protected $table = 'data_kelas';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+       
+    ];
+}
