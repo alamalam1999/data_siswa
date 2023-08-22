09009f1636a0b176d53086f85e9425cb173eeeb8
adi alam <adikuasa.mangkualam@ist.id>
2023-08-22 09:38:43 +0700

    add fixing display login

--- resources/views/frontend/layouts/auth.blade.php
+++ resources/views/frontend/layouts/auth.blade.php
@@ -58,9 +58,9 @@
 				<!--end::Body-->
 				{{-- <!--begin::Aside-->
 				<div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url({{ asset('assets/media/misc/auth-bg.png') }})">
-				{{-- empty --}}
-				</div>
-				<!--end::Aside--> --}}
+				 empty 
+				</div> --}}
+				<!--end::Aside-->
 			</div>
 			<!--end::Authentication - Sign-in-->
 		</div>
