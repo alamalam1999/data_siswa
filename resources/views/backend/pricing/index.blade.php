7be6b98565bf07cd6a374ce63ab97141466814b6
adi alam <adikuasa.mangkualam@ist.id>
2023-11-21 14:37:47 +0700

    Add Fixing

--- resources/views/backend/pricing/index.blade.php
+++ resources/views/backend/pricing/index.blade.php
@@ -16,7 +16,7 @@
             <div class="card-header">
                 <!--begin::Toolbar-->
                 <div class="card-toolbar">
-                    <a href="#" class="btn btn-light-success"><i class="bi bi-file-earmark-spreadsheet fs-4 me-2"></i> Download Template</a>
+                    <a href="{{ asset('uploads/file/data_siswa_new.xlsx') }}" class="btn btn-light-success"><i class="bi bi-file-earmark-spreadsheet fs-4 me-2"></i> Download Template</a>
                     <div class="d-flex align-items-center position-relative my-1 ms-15">
                         <input name="file_pricing" class="form-control" type="file" id="formFile" required>
                     </div>
@@ -27,28 +27,32 @@
             </div>
         </form>
 
+        <form action="{{ route('admin.import.dapodik') }}" method="POST" enctype="multipart/form-data">
+            {{ csrf_field() }}
             <div class="card-header">
                 <!--begin::Toolbar-->
                 <div class="card-toolbar">
-                    <form action="{{ route('admin.import.dapodik') }}" method="POST" enctype="multipart/form-data">
-                        {{ csrf_field() }}
-                        <a href="#" class="btn btn-light-success"><i class="bi bi-file-earmark-spreadsheet fs-4 me-2"></i> Download Template</a>
-                        <div class="d-flex align-items-center position-relative my-1 ms-15">
-                            <input name="file_dapodik" class="form-control" type="file" id="formFile" required>
-                        </div>
-                        <button id="btn-search" type="submit" class="btn btn-flex btn-primary ms-5">Upload Data Siswa DAPODIK<i class="bi bi-cloud-arrow-up fs-4 ms-4"></i></button>
-                    </form>
-                        <button id="btn-search" type="submit" class="btn btn-flex btn-primary ms-5">Data Dapodik Delete <i class="bi bi-cloud-arrow-up fs-4 ms-4"></i></button>
+                    <a href="{{ asset('uploads/file/new_check_dapodik.xlsx') }}" target="blank" class="btn btn-light-success"><i class="bi bi-file-earmark-spreadsheet fs-4 me-2"></i> Download Template</a>
+                    <div class="d-flex align-items-center position-relative my-1 ms-15">
+                        <input name="file_dapodik" class="form-control" type="file" id="formFile" required>
+                    </div>
+                    <button id="btn-search" type="submit" class="btn btn-flex btn-primary ms-5">Upload Data Siswa DAPODIK<i class="bi bi-cloud-arrow-up fs-4 ms-4"></i></button>
                 </div>
                 <!--end::Toolbar-->
             </div>
+        </form>
         <!--card-body-->
     </div>  
-    <div>
-        <a href="{{ route('admin.import.deletedapodik') }}"> Delete Dapodik</a>
+    <div class="card">
+        <p><strong>* Pastikan data import DATA SISWA sesuai Format</strong></p>
+
+        <p><strong>* Pastikan data import Dapodik NIS dan NISN Lengkap dan Terisi</strong></p>
     </div>
     <div>
-        <a href="{{ route('admin.import.deleteallsystem') }}">Delete All Siswa</a>
+       <strong><a href="{{ route('admin.import.deletedapodik') }}"> Delete Dapodik & PPDB</a></strong> 
+    </div>
+    <div>
+        <strong><a href="{{ route('admin.import.deleteallsystem') }}">Delete All Siswa</a></strong> 
     </div>
 </div>
 
